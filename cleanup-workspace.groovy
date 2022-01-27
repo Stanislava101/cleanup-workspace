211d695119850efe8dfd08cd5f76e1af6a5ab30d
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-01-27 15:09:52 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -17,7 +17,7 @@ for (job in Jenkins.instance.items)
     println "\n ***Job Name: "+job.name+"***"
         if(job.name =="cleanup-workspace"){
             println "testtt"
-            break;
+            return
         }
         if(job.workspace == null){
             println "nulll"
