2900c9406b2c221f14163c1a5e171db15c016d9f
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-01-28 11:33:52 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -47,7 +47,7 @@ for (job in Jenkins.instance.items)
                  a,b -> b.lastModified() <=> a.lastModified()
                  }
                 println "check test"
-                        if(it.isDirectory() == false){
+                if(isDirectory() == false){
                          println "There aren't any directories"
                      }
                  files.each{
