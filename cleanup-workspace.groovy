f7098c8049a626cda2e82a8d088caf26bbf38664
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-01-28 23:39:49 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -6,54 +6,42 @@ import hudson.model.*
 //manager.listener.logger.println new Date(System.currentTimeMillis()).format('MM/dd/yyyy hh:mm:ss a') + " / " + " -- Start Time" 
 
 //Get value from String Parameter
-MAX_BUILDS = 1
+MAX_BUILDS = 2
 
-    def list =[]
-        int count1 =0
 
 for (job in Jenkins.instance.items) 
 {
+  
   	int count = 0
-  	boolean check = false
+  	
     println "\n ***Job Name: "+job.name+"***"
-                    list.add(job.name)
         if(job.name =="cleanup-workspace"){
-         //   println "testtt"
+            println "testtt"
             continue;
         }
         if(job.workspace == null){
-            println "null"
+            println "nulll"
         }
-
-
+    
         if(job.workspace!=null && job.workspace!="")  //Check if there is a workspace associated with the Job
         {
-        String workspace = job.workspace
-        int workspaceLength = workspace.length()
-        int removeSymbol = workspaceLength -2
-  //          if(!(workspace.charAt(removeSymbol) == '@')){
-                long workspaceLength2 = job.workspace.length()
-                long fileSizeInKB = workspaceLength2/1024
-                println fileSizeInKB 
             println "Workspace path : " + job.workspace
-            println workspace.charAt(removeSymbol)
 
-           // String workspace = job.workspace
-             
+            String workspace = job.workspace
+            
             File folder = new File(workspace)
-          
+            
             if(folder!=null && folder.exists()) 
             {
-                 File[] files = new File(workspace).listFiles()
-                 files.sort{
-                 a,b -> b.lastModified() <=> a.lastModified()
-                 }
+                println "test"
 
+                 File[] files = new File(workspace).listFiles()
+                 //a,b -> b.lastModified().compareTo a.lastModified()           error
+                
+              //   }
                  files.each{
-                   //  println "Items are found"
-                   check =true
-                        if(it.isDirectory() == true) 
-                     {      println "in loop"
+                     if(!it.isFile())
+                     {
                          if(count < MAX_BUILDS){
                              println "test1"
                              println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " -- Save" 
@@ -66,18 +54,8 @@ for (job in Jenkins.instance.items)
                          }
                          count++
                      }
-                 
                  }
-             
-            if(check == true){
-                         println "Item found"
-                     }
-            else if(check == false){
-                println "Item not found"
-            }
-             
              }
-         //   }
             else
             {
                 println "Workspace is empty or doesn't exist"
@@ -89,11 +67,5 @@ for (job in Jenkins.instance.items)
         }
     }
 
-println "---------------------------"
-for(item in list){
-    println item
-}
-
-
-
+//build 60
 //manager.listener.logger.println new Date(System.currentTimeMillis()).format('MM/dd/yyyy hh:mm:ss a') + " / " + " -- End Time" 
