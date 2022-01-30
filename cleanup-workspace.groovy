9bca2a6665cad2e7f1a1e85a57b7d0be4d131671
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-01-30 22:15:38 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -2,115 +2,120 @@ import jenkins.*
 import jenkins.model.*
 import hudson.*
 import hudson.model.*
+import groovy.io.FileType
 
-long startTime = System.currentTimeMillis();
+//manager.listener.logger.println new Date(System.currentTimeMillis()).format('MM/dd/yyyy hh:mm:ss a') + " / " + " -- Start Time" 
 
 //Get value from String Parameter
-MAX_BUILDS = 3
+MAX_BUILDS = 2
 
-for (job in Jenkins.instance.items)
+ //   def list =[]
+  //      int count1 =0
+
+for (job in Jenkins.instance.items) 
 {
-    SortedMap<Integer,List<String>> sprint_paths_map  = new TreeMap<Integer, List<String>>(new DescOrder());
+  	int count = 0
+  	boolean check = false
+
+    println "\n ***Job Name: "+job.name+"***"
         if(job.name =="cleanup-workspace"){
             continue;
         }
-    println "\n ***** Job Name: "+job.name+" *****"
-    
+        if(job.workspace == null){
+            println "null"
+        }
+
+
         if(job.workspace!=null && job.workspace!="")  //Check if there is a workspace associated with the Job
         {
+        String workspace = job.workspace
+       // int workspaceLength = workspace.length()
+        //int removeSymbol = workspaceLength -2
+
+     //       if(!(workspace.charAt(removeSymbol) == '@')){
+         //       long workspaceLength2 = job.workspace.length()
+         //       long fileSizeInKB = workspaceLength2/1024
+          //      println fileSizeInKB 
             println "Workspace path : " + job.workspace
-            
-            String workspace = job.workspace
-            
-            File folder = new File(workspace)
-            
-            if(folder!=null && folder.exists()) //Check if the Workspace folder exists
+       //     println workspace.charAt(removeSymbol)
+            File workspaceFile = new File(workspace)
+
+            File folder = new File(workspace) 
+            println folder
+          
+            if(folder!=null && folder.exists()) 
             {
-                // Get all files and folders within the Workspace of current job. 
-                // Iterate through only folders and sort em by Modified Date.
+              //   println "${folder} ${folder.size()} ${new Date(folder.lastModified())}"
                 
-              	sprint_number = null
-              	
-              	//Determine which sprint_number to delete
-                File[] files = new File(workspace).listFiles().each{
-                  
-                  if(it!=null && it.exists() && !it.isFile()){
-				  
-                    sprint_number = it.name.find( /\d+/ )!=null ? it.name.find( /\d+/ ).toInteger() : null
-  
-                    if(sprint_number!=null)
-                    {
-                      if(sprint_paths_map.containsKey(sprint_number))
-                          sprint_paths_map.get(sprint_number).add(it.absolutePath)
-                      else
-                      {
-                          List<String> list = new ArrayList<String>()
-                          list.add(it.absolutePath);
-                          sprint_paths_map.put(sprint_number, list)
-                      }
-                    }
-                    sprint_number = null
-                  }
-                }
-              	
-              	int count = 0;
-              
-                //Delete the folders based on the build number
-				
-              	for(Map.Entry<Integer, List<String>> entry : sprint_paths_map.entrySet())
-              	{
-              		if(count >= MAX_BUILDS) {
-                  	//	deleteFolders(entry.getValue())
-                      println "Deleted"
-                    }
-                  	
-                    else {
-                      	println "Save - " + entry
-                    }
-                    
-                  count++
-                }
-              	
+                 File[] files = new File(workspace).listFiles()
+                
+                 files.sort{
+                 a,b -> b.lastModified() <=> a.lastModified()
+                 }
+
+
+                 files.each{
+                   check =true
+                        if(!it.isFile())         //isDirectory, it.isFile()
+                     {      
+                         if(count < MAX_BUILDS){
+                             println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " -- Save" 
+                         }
+                         else
+                         {
+                             println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " ** Deleted" 
+                            
+                         }
+                         count++
+                     }
+                     
+                 
+                 }
+             
+          /*  if(check == true){
+                         println "Item found"
+                     }
+            */
+            if(check == false){
+                println "Workspace is empty or doesn't exist"
             }
+             }
+            
             else
             {
                 println "Workspace is empty or doesn't exist"
             }
+         //   }
         }
         else
         {
-            println "No Workspace is associated with this job"
+            println "No Workspace associated with this job"
         }
-    
-}
-
-/*
-void deleteFolders(List<String> paths){
-  
-  println "To Delete"
-  
-  for(String path : paths)
-  {
-    File file = new File(path)
-    
-    if( ! file.isFile() && file.exists())
-    {
-        file.deleteDir()
-        .println "Deleted -" + path
     }
-  }
-}
-*/
-//For Descending order TreeMap
 
-class DescOrder implements Comparator<Integer> {
-	@Override
-	public int compare(Integer o1, Integer o2) {
-	    return o2.compareTo(o1);
-	}
-}
+    // def list2 = []
+    // def dir = new File("/storage/jenkins/workspace/validate-product-awsgc")
+    // dir.eachFileRecurse(FileType.FILES) {
+    //     file ->
+    //     list2 <<file
+    // }
+    // list.each{
+    //     println it.path
+    // }
 
-long endTime   = System.currentTimeMillis();
-long totalTime = (endTime - startTime)/1000;
 
-println "Total Run time in seconds : " + totalTime
+// def fileList = "ls -la /storage/jenkins/workspace/".execute().text
+// def files =[]
+// fileList.eachLine {
+// files.add(it)
+// }
+// for(it in files){
+//     println it
+// }
+
+
+// File dir = new File("/storage/jenkins/workspace")
+// dir.eachFile{f ->
+// println "${f} ${f.size()} ${new Date(f.lastModified())}"
+// }
+
