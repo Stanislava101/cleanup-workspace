14727e7241603108e6cbf9378acac572e29b3429
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-01-31 11:22:50 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -25,9 +25,9 @@ for (job in Jenkins.instance.items)
         if(job.name == "validate-product-ac2"){
             MAX_BUILDS = 5
         }
-        if(job.name == "validate-dashboards"){
-            MAX_BUILDS = 2
-        }
+        // if(job.name == "validate-dashboards"){
+        //     MAX_BUILDS = 2
+        // }
         if(job.name == "validate-concourse-pipeline"){
             MAX_BUILDS = 5
         }
