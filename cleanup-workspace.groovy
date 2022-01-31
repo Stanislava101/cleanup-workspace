6b8b71abfce0d73518010a7b217ba44687fae7d5
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-01-31 14:50:44 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -66,7 +66,7 @@ for (job in Jenkins.instance.items)
                      workspace = "/storage/jenkins/workspace/"+folder.getName().substring(0,nameLength)
                      println "The workspace is "
                      println workspace
-                     files = new File("/storage/jenkins/workspace/validate-product-aws@2")
+                     files = new File("/storage/jenkins/workspace/validate-product-aws")
                      
                                       files.sort{
                  a,b -> b.lastModified() <=> a.lastModified()
