d7935dbb055772544a765ff936f9bb0b532ef368
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-01-31 10:53:38 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -20,7 +20,7 @@ for (job in Jenkins.instance.items)
             continue;
         }
         if(job.name =="validate-iac-descriptors"){
-            MAX_BUILDS = 4
+            MAX_BUILDS = 2
         }
         if(job.workspace == null){
             println "null"
