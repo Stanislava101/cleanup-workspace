b22e1fc21fb074089c8ae05df12ae9b70fb20f15
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-01-31 23:07:26 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -39,10 +39,10 @@ for (job in Jenkins.instance.items)
                     println "remove @"
                     int length = folder.getName().length()
                     int nameLength = length - 2
-                  //   workspace = "/storage/jenkins/workspace/"+folder.getName().substring(0,nameLength)
+                     workspace = "/storage/jenkins/workspace/"+folder.getName().substring(0,nameLength)
                      println "The workspace is "
                      println workspace
-                     if(workspace == "/storage/jenkins/workspace/validate-product-aws2@2"){
+                     if(workspace == "/storage/jenkins/workspace/validate-product-aws2"){
                          println "aws3 found"
                          String dirs = workspace+"/50266"
                          files = new File(dirs)
