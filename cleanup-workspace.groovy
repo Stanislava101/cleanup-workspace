826a2e101a9142b7364583ecabdc5906930a1bd3
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-02 19:59:57 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -8,16 +8,10 @@ MAX_BUILDS = 3
 
 //Jenkins jenkins = Jenkins.instance
 //def jenkinsNodes = jenkins.nodes
-for (job in Jenkins.instance.items) 
+for (job in Jenkins.instance) 
 {
   	int count = 0
   	boolean check = false
-      for(job2 in Jenkins.instance){
-          def jenkinsNodes = job2.nodes
-          for(Node node in jenkinsNodes){
-              println "'$node.nodeName'"
-          }
-      }
     //  Jenkins jenkins2 = Jenkins.instance
     //  def jenkinsNodes = jenkins2.nodes
      // for(Node node in jenkinsNodes){
