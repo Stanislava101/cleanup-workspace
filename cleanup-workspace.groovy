aec208c7b73a8f840909fb4e44f6674f51cd1fff
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-02 19:43:46 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -13,7 +13,7 @@ for (job in Jenkins.instance.items)
   	int count = 0
   	boolean check = false
       Jenkins jenkins2 = Jenkins.instance
-      def jenkinsNodes = jenkins2.nodes
+      def jenkinsNodes = job.nodes
       for(Node node in jenkinsNodes){
           println"'$node.nodeName'"
       }
