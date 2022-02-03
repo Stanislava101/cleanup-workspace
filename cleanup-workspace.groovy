c9e72b8a138a6301dfbc0ab1cadcdced7a9cd966
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-03 13:16:46 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -33,13 +33,19 @@ for (job in Jenkins.instance.items)
 
             File folder = new File(workspace) 
             println folder
+              File[] files
              String folderString = folder.getName()
              int folderLength = folderString.length()
             int removeSymbol = folderLength -2
                             println folderString.charAt(removeSymbol)
                             println folder.getName()
+                        if(workspace == "/storage/jenkins/workspace/validate-product-aws3"){
+                         println "aws3 found"
+                         files = new File("/39325").listFiles()
+                         println files.getName()
 
-                 File[] files
+                     }
+
                 
                 if(folder.getName().charAt(removeSymbol) == '@'){
                     println "remove @"
@@ -103,12 +109,6 @@ for (job in Jenkins.instance.items)
 
                  files.each{
                    check =true
-                          if(workspace == "/storage/jenkins/workspace/validate-product-aws3"){
-                         println "aws3 found"
-                         files = new File("/39325").listFiles()
-                         println files.getName()
-
-                     }
                         if(!it.isFile())         //isDirectory, it.isFile()
                      {      
                          if(count < MAX_BUILDS){
