3bd422693bad3eea59860952d2dfbd9a5e08bf98
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-03 11:38:34 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -1,78 +1,31 @@
-// Check if a slave has < 10 GB of free space, wipe out workspaces if it does
-
-import hudson.model.*;
-import hudson.util.*;
 import jenkins.model.*;
-import hudson.FilePath.FileCallable;
-import hudson.slaves.OfflineCause;
-import hudson.node_monitors.*;
 
+for (item in Jenkins.instance.items) {
+  jobName = item.getFullDisplayName()
+   if(!item.isBuilding()) {
+     if (jobName.startsWith(JOB_PREFIX)) {
+       println("Wiping out workspaces of job " + jobName)
+       customWorkspace = item.getCustomWorkspace()
 
-def performCleanup(def node, def items) {
-  
-  for (item in items) {
-    jobName = item.getFullDisplayName()
-    
-    println("Cleaning " + jobName)
-    
-    if(item instanceof com.cloudbees.hudson.plugins.folder.AbstractFolder) {
-      	performCleanup(node, item.items)
-    	continue
-    }
-    
-    if (item.isBuilding()) {
-      println(".. job " + jobName + " is currently running, skipped")
-      continue
-    }
-    
-    println(".. wiping out workspaces of job " + jobName)
-
-    workspacePath = node.getWorkspaceFor(item)
-    if (workspacePath == null) {
-      println(".... could not get workspace path")
-      continue
-    }
-    
-    println(".... workspace = " + workspacePath)
-    
-    pathAsString = workspacePath.getRemote()
-    if (workspacePath.exists()) {
-      String workspace = workspacePath
-      File folder = new File(workspace)
-      if(folder!=null && folder.exists()){
-        File[] files = new File(workspace).listFiles()
-        files.each{
-          println "in loop"
-          if(it.isFile() == true){
-         println it.name + "---S"
-          }else if (it.isDirectory() == true){
-            println it.name + "----D"
-          }
-        }
-       println folder
-      }
-    //  workspacePath.deleteRecursive()
-      println(".... deleted from location " + pathAsString)
-    } else {
-      println(".... nothing to delete at " + pathAsString)
-    }
-  }  
-}
-
-
-for (node in Jenkins.instance.nodes) {
-    computer = node.toComputer()
-    if (computer.getChannel() == null) continue
-
-    rootPath = node.getRootPath()
-    size = DiskSpaceMonitor.DESCRIPTOR.get(computer).size
-    roundedSize = size / (1024 * 1024 * 1024) as int
-
-    println("node: " + node.getDisplayName() + ", free space: " + roundedSize + "GB")
-    computer.setTemporarilyOffline(true, new hudson.slaves.OfflineCause.ByCLI("disk cleanup"))
-  
-    performCleanup(node, Jenkins.instance.items)
-  
-    computer.setTemporarilyOffline(false, null)
-
+       for (node in Jenkins.getInstance().getNodes())
+       {
+         nodeName = node.getDisplayName()
+         workspacePath = node.getWorkspaceFor(item)
+         if (customWorkspace != null)
+         {
+           workspacePath = node.getRootPath().child(customWorkspace)
+         }
+         if (workspacePath != null)
+         {
+           pathAsString = workspacePath.getRemote()
+           if (workspacePath.exists())
+           {
+             println("  [" + nodeName + "] Deleting " + pathAsString)
+          //   workspacePath.deleteRecursive()
+             println("  [" + nodeName + "] Deleted from location " + pathAsString)
+           }
+         }
+       }
+     }
+   }
 }
