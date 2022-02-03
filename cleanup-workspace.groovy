f930afec20cbff24856363a8d8a6b3b3afecfe8a
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-03 10:47:28 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -40,7 +40,7 @@ def performCleanup(def node, def items) {
       String workspace = workspacePath
       File folder = new File(workspace)
       if(folder!=null && folder.exists()){
-        File files = (File[]) new File(workspace).listFiles()
+        File files = new File []
         files.each{
          println it.name
         }
