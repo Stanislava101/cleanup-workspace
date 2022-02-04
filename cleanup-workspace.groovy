8c2b6d6cf4b81ffadc2661cf2dd6d56c90f0c95c
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-04 14:43:12 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -96,4 +96,35 @@ for (item in Hudson.instance.items)
         }
       }
     }
+        if (jobName == "validate-dashboards")
+    {
+      println("Wiping out workspaces of job " + jobName)
+      
+      for (node in Hudson.getInstance().getNodes())
+      {
+        println("  Node: " + node.getDisplayName())
+        workspacePath = node.getWorkspaceFor(item)
+        if (workspacePath == null)
+        {
+          println("    Could not get workspace path")
+        }
+        else
+        {
+          pathAsString = workspacePath.getRemote()
+          if (workspacePath.exists())
+          {
+            File[] files = new File(pathAsString).listFiles()
+            files.each{
+              println it
+            }
+          //  workspacePath.deleteRecursive()
+            println("    Deleted from location " + pathAsString)
+          }
+          else
+          {
+            println("    Nothing to delete at " + pathAsString)
+          }
+        }
+      }
+    }
 }
