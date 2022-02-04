4168bbd4d1a5119215b6fb826cfe21447e6ce758
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-04 18:49:39 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -2,175 +2,48 @@ import jenkins.*
 import jenkins.model.*
 import hudson.*
 import hudson.model.*
-import groovy.io.FileType
 
-MAX_BUILDS = 20
 
+//Get value from String Parameter
+MAX_BUILDS = 5
 
 for (job in Jenkins.instance.items) 
 {
-
+  
   	int count = 0
-  	boolean check = false
-
+  	
     println "\n ***Job Name: "+job.name+"***"
-        if(job.name =="cleanup-workspace"){
-            continue;
-        }
-
-        // if(job.name == "validate-dashboards"){
-        //                  println "dashboards found"
-        //                  String w = job.workspace
-        //               //   File files = new File(w).listFiles()
-        //                 File f = new File("/storage/jenkins/workspace/validate-dashboards/products")
-        //                 if(f == null){
-        //                   println "folder is null"
-        //                 }else if (f.exists() == false){
-        //                   println f
-        //                   println "Folder doesn't exists"
-        //                 } else{
-        //                   println "weird"
-        //                   File theF = new File("/storage/jenkins/workspace/validate-dashboards/products").listFiles()
-        //                   theF.each{
-        //                     println it.name + " ---S"
-        //                   }
-        //                 }
-        //              }
-
+    
         if(job.workspace!=null && job.workspace!="")  //Check if there is a workspace associated with the Job
         {
-
-        String workspace = job.workspace
-
             println "Workspace path : " + job.workspace
-
-            File folder = new File(workspace) 
-            println folder
-              File[] files
-             String folderString = folder.getName()
-             int folderLength = folderString.length()
-            int removeSymbol = folderLength -2
-                            println folderString.charAt(removeSymbol)
-                            println folder.getName()
-            if(workspace == "/storage/jenkins/workspace/validate-dashboards"){
-                         println "dashboards found"
-                       //  files = new File("/storage/jenkins/workspace/validate-product-aws3/39326").listFiles()
-                      //  files = new File("/storage/jenkins/workspace/validate-product-aws3/39344").listFiles()
-                        File f = new File("/storage/jenkins/workspace/validate-product-aws3/39340")
-                        if(f.isFile() == true){
-                          println "True"
-                        } else if (f.isDirectory() == true){
-                          println "False"
-                        }
-                         files.each{
-                           println it.name
-                         }
-                           files.each{
-                           println it
-                         }
-
-                     }
-
-                
-                if(folder.getName().charAt(removeSymbol) == '@'){
-                    println "remove @"
-                    int length = folder.getName().length()
-                    int nameLength = length - 2
-                     workspace = "/storage/jenkins/workspace/"+folder.getName().substring(0,nameLength)
-                     println "The workspace is "
-                     println workspace
-                     if(workspace == "/storage/jenkins/workspace/validate-product-aws3"){
-                         println "aws3 found"
-                       //  files = new File("/storage/jenkins/workspace/validate-product-aws3/39326").listFiles()
-                      //  files = new File("/storage/jenkins/workspace/validate-product-aws3/39344").listFiles()
-                        File f = new File("/storage/jenkins/workspace/validate-product-aws3/39340")
-                        if(f.isFile() == true){
-                          println "True"
-                        } else if (f.isDirectory() == true){
-                          println "False"
-                        }
-                         files.each{
-                           println it.name
-                         }
-                           files.each{
-                           println it
-                         }
-
-                     }
-                   //  files = new File(workspace).listFiles()
-                     println "${workspace} ${workspace.size()}"
-                     for(file in files){
-                         println "file is " + file
-                     }
-
-                     println "with @"
-                long workspaceLength2 = job.workspace.length()
-                long fileSizeInKB = workspaceLength2/1024
-                println fileSizeInKB 
-                         
-
-                //                       files.sort{
-                //  a,b -> b.lastModified() <=> a.lastModified()
-                //  }
-                files.each{
-                    println "Path is " + it
-                }
-
-
-                 files.each{
-                   check =true
-                        if(!it.isFile())         //isDirectory, it.isFile()
-                     {      
-                         if(count < MAX_BUILDS){
-                             println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " -- Save" 
-                         }
-                         else
-                         {
-                             println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " ** Deleted" 
-                            
-                         }
-                         count++
-                     }
-                     
-                 
-                 }
-             }
-          
-            if(folder!=null && folder.exists()) 
-            {
-                 files = new File(workspace).listFiles()
-                                println ("without @")
-                 files.sort{
-                 a,b -> b.lastModified() <=> a.lastModified()
-                 }
-
-                 files.each{
-                   check =true
-                        if(!it.isFile())         //isDirectory, it.isFile()
-                     {      
-                         if(count < MAX_BUILDS){
-                             println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " -- Save" 
-                         }
-                         else
-                         {
-                             println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " ** Deleted" 
-                            
-                         }
-                         count++
-                     }
-                     
-                 
-                 }
-             
-          /*  if(check == true){
-                         println "Item found"
-                     }
-            */
-            if(check == false){
-                println "Workspace is empty or doesn't exist"
-            }
-             }
             
+            String workspace = job.workspace
+            
+            File folder = new File(workspace)
+            
+            if(folder!=null && folder.exists()) //Check if the Workspace folder exists
+            {
+                // Get all files and folders within the Workspace of current job. 
+                //Iterate through only folders and sort em by Modified Date.
+                
+                File[] files = new File(workspace).listFiles().sort(){
+                a,b -> b.lastModified() <=> a.lastModified()
+                }
+                .each{
+                    if(!it.isFile()) //Check only for folders
+                    {
+                        if(count < MAX_BUILDS)
+                            println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " -- Save" 
+                        else
+                        {
+                            println new Date(it.lastModified()).format('MM/dd/yyyy hh:mm:ss a') + " /" + it.name + " ** Deleted" 
+                        //    it.deleteDir()
+                        }
+                        count++
+                    }
+                }
+            }
             else
             {
                 println "Workspace is empty or doesn't exist"
@@ -181,3 +54,5 @@ for (job in Jenkins.instance.items)
             println "No Workspace associated with this job"
         }
     }
+
+}
