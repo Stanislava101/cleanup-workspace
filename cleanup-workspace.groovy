da3eaf09a9d6f0e2fb5dc75427bc573482dd2833
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-04 11:44:46 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -1,126 +1,46 @@
-
 import hudson.model.*
-import hudson.node_monitors.*
-import hudson.slaves.*
-import hudson.FilePath
-import java.util.concurrent.*
+// For each job
+for (item in Hudson.instance.items)
+{
+  jobName = item.getFullDisplayName()
+  // check that job is not building
+  //if (!item.isBuilding())
+  //{
+    // TODO: Modify the following condition to select which jobs to affect
+    if (jobName == "clean_jenkins_machines")
+    {
+      println("Wiping out workspaces of job " + jobName)
+      customWorkspace = item.getCustomWorkspace()
+      println("Custom workspace = " + customWorkspace)
+      
+      for (node in Hudson.getInstance().getNodes())
+      {
+        println("  Node: " + node.getDisplayName())
+        workspacePath = node.getWorkspaceFor(item)
+        if (workspacePath == null)
+        {
+          println("    Could not get workspace path")
+        }
+        else
+        {
+          if (customWorkspace != null)
+          {
+            workspacePath = node.getRootPath().child(customWorkspace)
+          }
 
-jenkins = Hudson.instance
-
-// Define day to compare (will find builds that were built more than n day ago)
-day=23;
-hour=24;
-minute=60;
-second=60;
-daysInSecond=day*hour*minute*second;
-now=Calendar.instance;
-list=[];
-listFreezeBuild=[];
-listProceeded=[];
-
-println("The build is run at ${now.time}");
-
-println("\n### GET LIST OF ALL JOBS ###\n")
-
-for (item in jenkins.items) {
-  println("\tProcessing ${item.name}...")
-  // ignore project that contains freeze or patch case insensitive
-  if (item.name ==~ /(?i)(freeze|patch).*/) {
-    // add item to list
-    listFreezeBuild << item;
-  } else if (!item.disabled&&item.getLastBuild()!=null) {
-    // caculate build time
-    build_time=item.getLastBuild().getTimestamp();
-    // compare build time with current time
-    if (now.time.time/1000-build_time.time.time/1000>daysInSecond) {
-      // add item to list
-      list << item;
-    }
-  }
-}
-
-println("\n### DISABLE OLD JOBS ###\n")
-
-def disableJob(item) {
-  if (item.class.canonicalName != 'com.cloudbees.hudson.plugins.folder.Folder') {
-    // disable item
-    item.disabled=true;
-    // save
-    item.save();
-  }
-}
-
-def pushLogRotate(item) {
-  // perform log rotation
-  item.logRotate();
-}
-
-def wipeOutWorkspace(item) {
-  // check if build is not in building stage
-  if(!item.isBuilding()) {
-    try {
-      // wipe out the workspace
-      item.doDoWipeOutWorkspace();
-    }
-    catch (ex) {
-      println("Error: " + ex);
-    }
-  }
-}
-
-def wipeOutWorkspaceFromSlaves(item) {
-  for (slave in hudson.model.Hudson.instance.slaves) {
-    // get slave name
-    slaveNodeName = slave.getNodeName();
-    println("\t\tCheck ${slaveNodeName} slave...");
-    // get workspace root for specify slave
-    workspaceRoot = slave.getWorkspaceRoot();
-    // create path slave with workspace
-    FilePath fp = slave.createPath(workspaceRoot.toString() + File.separator + item.name);
-    // check that workspace root exists and path to workspace exists
-    if ((workspaceRoot != null) && (fp.exists())) {
-      // delete workspace recursively
-      println("\t\tWipe out ${fp}...")
-      try {
-      //  fp.deleteRecursive();
-      println "delete"
-      }
-      catch (ex) {
-        println("Error: " + ex);
+          pathAsString = workspacePath.getRemote()
+          if (workspacePath.exists())
+          {
+           // workspacePath.deleteRecursive()
+            println("    Deleted from location " + pathAsString)
+          }
+          else
+          {
+            println("    Nothing to delete at " + pathAsString)
+          }
+        }
       }
     }
-  }
+  //}
+ 
 }
-
-if (list.size() > 0) {
-  for (item in list) {
-    println("\tDisabling ${item.name}...");
-  //  disableJob(item);
-    println("\tPush log rotate for ${item.name}...");
-    pushLogRotate(item);
-    println("\tWipe out workspace for ${item.name}...");
-   // wipeOutWorkspace(item);
-    println("\tWipe out workspace for ${item.name} from slaves...")
-    wipeOutWorkspaceFromSlaves(item);
-    // add item to list
-    listProceeded << item;
-  }
-}
-
-println("\n### SUMMARY INFORMATION ###\n");
-
-if (listFreezeBuild.size() > 0) {
-  println("Next jobs were ignored as it is a freeze or patch build:")
-  for (item in listFreezeBuild) {
-     println("\t${item.name}");
-  }
-}
-
-if (listProceeded.size() > 0) {
-  println("Next jobs were procced, disabled and cleaned workspace:")
-  for (item in listProceeded) {
-     println("\t${item.name}");
-  }
-}
-
-return 0;
