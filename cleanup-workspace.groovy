ceffde09b2eddba8766d4e82c25e2ebe45ab0886
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2022-02-04 11:14:18 +0200

    Update

--- cleanup-workspace.groovy
+++ cleanup-workspace.groovy
@@ -1,5 +1,3 @@
-// Check if a slave has < 10 GB of free space, wipe out workspaces if it does
-
 import hudson.model.*;
 import hudson.util.*;
 import jenkins.model.*;
@@ -7,75 +5,121 @@ import hudson.FilePath.FileCallable;
 import hudson.slaves.OfflineCause;
 import hudson.node_monitors.*;
 
+//threshold is in GB and comes from a job parameter
+def threshold = Integer.parseInt(build.buildVariableResolver.resolve("CLEAN_THRESHOLD"))
+def skippedLabels = [ 'container' ] //don't clean docker slaves
+def extraDirectoriesToDelete = [ 'temp' ] //additional paths under slave's root path that should be removed if found
 
-def performCleanup(def node, def items) {
-  
-  for (item in items) {
-    jobName = item.getFullDisplayName()
-    
-    println("Cleaning " + jobName)
-    
-    if(item instanceof com.cloudbees.hudson.plugins.folder.AbstractFolder) {
-      	performCleanup(node, item.items)
-    	continue
-    }
-    
-   // if (item.isBuilding()) {
-    //  println(".. job " + jobName + " is currently running, skipped")
-    //  continue
-   // }
-    
-    println(".. wiping out workspaces of job " + jobName)
-    
-    workspacePath = node.getWorkspaceFor(item)
-    if (workspacePath == null) {
-      println(".... could not get workspace path")
-      continue
-    }
-    
-    println(".... workspace = " + workspacePath)
-    
-    pathAsString = workspacePath.getRemote()
-    if (workspacePath.exists()) {
-            String w = workspacePath
 
-      File folder = new File(w)
-      if(folder == null){
-        println "folder is null"
-        println folder
-      }else if(folder.exists() == false){
-        println "folder doesn't exist"
-        println folder
-      }else{
-      File[] files = new File(w).listFiles()
-      files.each{
-        println it.name
+def deleteRemote(def path, boolean deleteContentsOnly) {
+  boolean result = true
+  def pathAsString = path.getRemote()
+  if (path.exists()) {
+    try {
+      if (deleteContentsOnly) {
+   //     path.deleteContents()
+        println ".... deleted ALL contents of ${pathAsString}"
+      } else {
+     // 	path.deleteRecursive()
+        println ".... deleted directory ${pathAsString}"
       }
-  
-
-     // workspacePath.deleteRecursive()
-      println(".... deleted from location " + pathAsString)
-      }
-    } else {
-      println(".... nothing to delete at " + pathAsString)
+    } catch (Throwable t) {
+      println "Failed to delete ${pathAsString}: ${t}"
+      result = false
     }
-  }  
+  }
+  return result
 }
 
 
-for (node in Jenkins.instance.nodes) {
-    computer = node.toComputer()
-    if (computer.getChannel() == null) continue
+def failedNodes = []
 
-    rootPath = node.getRootPath()
+for (node in Jenkins.instance.nodes) {
+  computer = node.toComputer()
+  if (computer.getChannel() == null) {
+    continue
+  }
+      
+  if (node.assignedLabels.find{ it.expression in skippedLabels }) {
+    println "Skipping ${node.displayName} based on labels"
+    continue
+  }
+
+  try {
     size = DiskSpaceMonitor.DESCRIPTOR.get(computer).size
     roundedSize = size / (1024 * 1024 * 1024) as int
 
-    println("node: " + node.getDisplayName() + ", free space: " + roundedSize + "GB")
-    computer.setTemporarilyOffline(true, new hudson.slaves.OfflineCause.ByCLI("disk cleanup"))
-  
-    performCleanup(node, Jenkins.instance.items)
-  
-    computer.setTemporarilyOffline(false, null)
+    println("node: " + node.getDisplayName() + ", free space: " + roundedSize + "GB. Idle: ${computer.isIdle()}")
+    if (roundedSize < threshold) {
+      def prevOffline = computer.isOffline()
+      if (prevOffline && computer.getOfflineCauseReason().startsWith('disk cleanup from job')) {
+        prevOffline = false //previous run screwed up, ignore it and clear it at the end
+      }
+      if (!prevOffline) {
+        //don't override any previosly set temporarily offline causes (set by humans possibly)
+      	computer.setTemporarilyOffline(true, new hudson.slaves.OfflineCause.ByCLI("disk cleanup from job ${build.displayName}"))
+      }
+      if (computer.isIdle()) {
+        //It's idle so delete everything under workspace
+        def workspaceDir = node.rootPath.child('workspace')
+        if (!deleteRemote(workspaceDir, true)) {
+          failedNodes << node
+        }
+        
+        //delete custom workspaces
+        Jenkins.instance.getAllItems(TopLevelItem).findAll{item ->  item instanceof Job && !("${item.class}".contains('WorkflowJob')) && item.getCustomWorkspace()}.each{ item ->
+          if (!deleteRemote(node.getRootPath().child(item.customWorkspace), false)) {
+            failedNodes << node
+          }
+        }
+        
+        extraDirectoriesToDelete.each{
+          if (!deleteRemote(node.getRootPath().child(it), false)) {
+            failedNodes << node
+          }
+        }
+        
+      } else {
+      
+      	Jenkins.instance.getAllItems(TopLevelItem).findAll{item ->  item instanceof Job && !item.isBuilding() && !("${item.class}".contains('WorkflowJob')) }.each{ item ->
+          jobName = item.getFullDisplayName()
+          
+          
+          //println(".. checking workspaces of job " + jobName)
+          
+          workspacePath = node.getWorkspaceFor(item)
+          if (!workspacePath) {
+            println(".... could not get workspace path for ${jobName}")
+            return
+              }
+          
+          //println(".... workspace = " + workspacePath)
+          
+          customWorkspace = item.getCustomWorkspace()
+          if (customWorkspace) {
+            workspacePath = node.getRootPath().child(customWorkspace)
+            //    println(".... custom workspace = " + workspacePath)
+          }
+          
+          if (!deleteRemote(workspacePath, false)) {
+            failedNodes << node
+          }
+        }
+      }
 
+      if (!prevOffline) {
+        computer.setTemporarilyOffline(false, null)
+      }
+    }
+  } catch (Throwable t) {
+    println "Error with ${node.displayName}: ${t}"
+    failedNodes << node
+    
+  }
 }
+
+println "\n\nSUMMARY\n\n"        
+failedNodes.each{node ->
+  println "\tERRORS with: ${node.displayName}"
+}
+assert failedNodes.size() == 0
